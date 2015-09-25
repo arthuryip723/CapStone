6b0d1eb813d37bf16b0c79024e16852a6b28b653
App Academy Student <appacademy@Apps-Mac-mini-67.local>
2015-09-25 13:03:59 -0400

    appearance improvement

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,6 +7,7 @@ class Book < ActiveRecord::Base
   has_attached_file :image, default_url: "missing.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   def average_rating
-    reviews.average('rating')
+    average_rating = reviews.average('rating')
+    average_rating ? average_rating.round(2) : average_rating
   end
 end
